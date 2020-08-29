0b0e6d2e5175d9f18a5e5469e893f1fee0a635f1
Marc <mlanglois333@icloud.com>
2020-08-29 10:55:11 -0400

    Created routing functions

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const Customer = require("./customer");
 const wait = [];
-const table =[];
+const table = [];
 
-function waitlist(cust) {wait.push(cust);}
-function tablelist(cust) {table.push(cust);}
+function waitlist(cust) { wait.push(cust); }
+
+function tablelist(cust) { table.push(cust); }
+
+function reserve() {
+    const tempName = $("#name").val();
+    const tempPhone = $("#phone").val();
+    const tempId = $("#id").val();
+    const newCust = new Customer(tempName, tempPhone, tempId);
+
+    if (table.length > 4) {
+        waitlist(newCust)
+    }
+    else {
+        tablelist(newCust)
+    };
+
+}
